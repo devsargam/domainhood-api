255eff83e200e86d5199f127af3831b945ce99c5
Sargam Poudel <sargampoudel100@gmail.com>
2024-12-29 22:58:09 +0530

    feat: prisma init

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { UserService } from './user/user.service';
+import { User as UserModel } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly userService: UserService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/health')
+  healthCheck(): { status: string; message: string } {
+    return {
+      status: 'ok',
+      message: 'Server is running',
+    };
+  }
+
+  @Post('user')
+  async signupUser(
+    @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
   }
 }
